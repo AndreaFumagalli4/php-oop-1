2391d460627864154bc5736edce1972bf573f691
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-03 15:24:10 +0100

    add construct function

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,10 +1,16 @@
 <?php 
-
 class Movie {
 
     public $name;
     public $genre;
     public $mainCharacter;
+
+    function __construct($_name, $_genre, $_mainCharacter)
+    {
+        $this->name = $_name;
+        $this->genre = $_genre;
+        $this->mainCharacter = $_mainCharacter;
+    }
 
 }
 
